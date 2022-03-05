9a18589bae3401686e9e2ade4c7ab35413ab07f0
Mateusz Kruszewski <mat.kruszewski95@gmail.com>
2022-03-05 09:57:20 +0100

    Dodaj pliki projektów.

---
+++ PhoneBook.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    class PhoneBook
+    {
+
+        public List<Contact> Contacts { get; set; } = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+
+        public void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"{contact.Name} {contact.Number}");
+        }
+        public void DisplayContactByNumber(string number)
+        {
+            var contact = Contacts.FirstOrDefault(c => c.Number == number);
+            if (number == null)
+            {
+                Console.WriteLine("Cannot found contact");
+            }
+            else
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void DisplayAllContacts()
+        {
+            foreach (var contact in Contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void DisplayContactByName(string name)
+        {
+            var nameContact = Contacts.Where(c => c.Name.Contains(name)).ToList();
+            foreach (var contacts in nameContact)
+            {
+                DisplayContactDetails(contacts);
+            }
+
+        }
+        public void RemoveContactByNumber(string number)
+        {
+            var contact = Contacts.FirstOrDefault(c => c.Number == number);
+            if (number == null)
+            {
+                Console.WriteLine("Cannot found contact");
+            }
+            else
+            {
+                Contacts.Remove(contact);
+            }
+        }
+
+        
+        public void DisplayInstruction() 
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press 1 to add contact");
+            Console.WriteLine("Press 2 to display contact by number");
+            Console.WriteLine("Press 3 to search contact");
+            Console.WriteLine("Press 4 to display all contacts");
+            Console.WriteLine("Press 5 to Remove contact by number");
+            Console.WriteLine("Press 0 to exit");
+            Console.WriteLine();
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+namespace PhoneBook
+{
+    class Program : PhoneBook
+    {
+        static void Main(string[] args)
+        {
+            PhoneBook phoneBook;
+            bool isSerializedPhoneBook =
+                File.Exists(@"C:\Users\matkr\source\repos\PhoneBook\JSON\phoneBookSerialized.json");
+            if (!isSerializedPhoneBook)
+            {
+                phoneBook = new PhoneBook();
+            }
+            else
+            {
+                string serializedPhoneBook =
+                    File.ReadAllText(@"C:\Users\matkr\source\repos\PhoneBook\JSON\phoneBookSerialized.json");
+
+                phoneBook = JsonConvert.DeserializeObject<PhoneBook>(serializedPhoneBook);
+            }
+
+
+
+
+            Console.WriteLine("Welcome in the phone book");
+
+            phoneBook.DisplayInstruction();
+
+            var UserInput = Console.ReadLine();
+
+
+            while (true)
+            {
+                switch (UserInput)
+                {
+                    case "1":
+                        Console.WriteLine("Insert number:");
+                        var number = Console.ReadLine();
+                        while (UserInput.Length != 9)
+                        {
+                            if (number.Length != 9)
+                            {
+                                Console.WriteLine("Contact number must have 9 digits.");
+
+                                Console.WriteLine("Insert number:");
+                                number = Console.ReadLine();
+                            }
+                            else
+                            {
+                                break;
+                            }
+
+                        }
+                        Console.WriteLine("Insert name:");
+                        var name = Console.ReadLine();
+                        while (UserInput.Length < 3)
+                        {
+                            if (name.Length < 3)
+                            {
+                                Console.WriteLine("Contact name must have 3 or more letters.");
+
+                                Console.WriteLine("Insert name:");
+                                name = Console.ReadLine();
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+
+                        var newContact = new Contact(name, number);
+
+                        phoneBook.AddContact(newContact);
+                        Console.WriteLine($"Contact {name} has been added");
+
+                        break;
+                    case "2":
+
+                        Console.WriteLine("Insert number:");
+                        var searchNumber = Console.ReadLine();
+
+                        phoneBook.DisplayContactByNumber(searchNumber);
+
+                        break;
+                    case "3":
+                        Console.WriteLine("Insert name:");
+                        var searchName = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.WriteLine("Contact:");
+                        phoneBook.DisplayContactByName(searchName);
+
+
+                        break;
+                    case "4":
+                        Console.WriteLine("Your contact List:");
+
+                        phoneBook.DisplayAllContacts();
+                        break;
+                    case "5":
+
+                        Console.WriteLine("Insert number:");
+                        var removeNumber = Console.ReadLine();
+
+
+                        phoneBook.RemoveContactByNumber(removeNumber);
+                        Console.WriteLine($"Contact {removeNumber} has been removed.");
+
+
+                        break;
+                    case "0":
+                        string phoneBookSerialized = JsonConvert.SerializeObject(phoneBook);
+                        File.WriteAllText(@"C:\Users\matkr\source\repos\PhoneBook\JSON\phoneBookSerialized.json", phoneBookSerialized);
+                        Console.WriteLine("Changes saved and application closed");
+                        return;
+                    default:
+                        Console.WriteLine("Wrong Input");
+                        break;
+
+                }
+                phoneBook.DisplayInstruction();
+                UserInput = Console.ReadLine();
+            }
+
+
+
+
+        }
+    }
+}
